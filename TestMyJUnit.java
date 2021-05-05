ba63995e3b90276b3536ac68e65b349326e9634a
ashishaggarwal03 <39592820+ashishaggarwal03@users.noreply.github.com>
2021-05-05 13:11:31 +0530

    Add files via upload
    This is my TestMyJUnit file.

---
+++ TestMyJUnit.java
@@ -0,0 +1,53 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+/*import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+*/
+public class TestMyJUnit {
+
+	@Test
+	public void testWelcome1() {
+		String welcomeMessage = "Welcome to JUnit";
+		GreetMessenger greetmsg = new GreetMessenger("Welcome to JUnit");
+		assertEquals("Welcome to JUnit", greetmsg.getGreeting());
+
+	}
+
+	@Test
+	public void testWelcome2() {
+		String welcomeMessage = "Welcome to JUnit";
+		assertEquals("Welcome to jUnit", welcomeMessage);
+
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void Bank() {
+		SavingsAccount sa = new SavingsAccount(15000);
+		assertEquals(69980, sa.getBalance());
+	}
+
+	@Test
+	public void testSomeAssertions() {
+		int empno = 7839;
+		String empName = "Ashish";
+		String empJob = "Manager";
+
+		assertEquals("Manager", empJob);
+		System.out.println("EmpJob has passed....");
+		assertFalse(empno < 1000);
+		System.out.println("Empno is passed....");
+		assertNotNull(empName);
+		System.out.println("Empname is passed....");
+		System.out.println("testSomeAssertions is over....");
+
+	}
+
+}
