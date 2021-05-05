178bf272098c0fcac6510c4baebfef2bd2c0ae94
ashishaggarwal03 <Aggarwalashish317@gmail.com>
2021-05-05 18:06:17 +0530

    This is a JUnit project.

---
+++ JUnit Project/src/GreetMessenger.java
@@ -0,0 +1,20 @@
+
+public class GreetMessenger {
+
+	private String greeting;
+	
+		GreetMessenger(String greet){
+			this.greeting=greet;
+			
+		}
+		public String getGreeting() {
+			System.out.println("Greeting : " +greeting);
+			return greeting;
+			
+		}
+		
+		
+	
+	
+	
+}
